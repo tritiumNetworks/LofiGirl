1d1111d758784f1edb7d2eae4710bb977a60800c
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-10-08 17:09:26 +0900

    Fix cache system

--- utils/getTrack.js
+++ utils/getTrack.js
@@ -1,27 +1,21 @@
 const { get } = require('superagent')
-let cache
+const cache = {}
 
 /**
  * @param {import('lavacord').LavalinkNode} node
  * @param {string} search
  */
 async function getSongs (node, search) {
-  const params = new URLSearchParams()
-  params.append('identifier', search)
-  const res =
-    await get('http://' + node.host + ':' + node.port + '/loadtracks?' + params)
-      .set('Authorization', node.password)
+  if (!cache[search]) {
+    const params = new URLSearchParams()
+    params.append('identifier', search)
+    const res =
+      await get('http://' + node.host + ':' + node.port + '/loadtracks?' + params)
+        .set('Authorization', node.password)
 
-  return res.body
+    cache[search] = res.body
+    return res.body
+  } else return cache[search]
 }
 
-module.exports = async (node, url) => {
-  if (!cache) {
-    cache = (await getSongs(node, url)).tracks[0].track
-    setTimeout(() => {
-      cache = undefined
-    }, 600000)
-  }
-
-  return cache
-}
+module.exports = getSongs
